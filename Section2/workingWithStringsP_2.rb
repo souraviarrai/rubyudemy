0b55cc36d68c6ae5f9e1f7e08ed4d59dd971d859
Sourav Rai <109982445+souraviarrai@users.noreply.github.com>
2023-05-24 16:28:04 +0000

    part2 of string

---
+++ Section2/workingWithStringsP_2.rb
@@ -0,0 +1,24 @@
+=begin
+puts "What is your name:"
+name = gets.chomp
+puts "Ok, so your name is #{name}"
+puts name * 2
+puts "\t"
+=end
+=begin
+puts "Enter a number to multiply by 2: "
+input = gets.chomp
+puts input.to_i * 2
+=end
+
+# home work assignment
+
+puts "Enter your first name"
+first_name = gets.chomp
+puts "Enter your last name"
+last_name = gets.chomp
+full_name = "#{first_name} #{last_name}"
+puts "Your full name is #{full_name}"
+puts "Your full name reversed is #{full_name.reverse}"
+puts "Your name has #{full_name.length} characters in it"
+
